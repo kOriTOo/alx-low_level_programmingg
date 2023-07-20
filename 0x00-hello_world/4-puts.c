9cff944bf935d1d61cc728cf6d7a62fdb5d8c945
kOriTO <amr01000620@gmail.com>
2023-07-20 04:35:07 -0700

    saved

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
-/*yo yo*/
+/**
+ * main - Entry point
+ *
+ *Description: prints "programming is like building a multiligual puzzel"
+ *
+ *Return: Always 0 (Success)
+ *
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
