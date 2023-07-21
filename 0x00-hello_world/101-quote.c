9ee976b547ccb64a4fd39b294d786b5361829323
kOriTO <amr01000620@gmail.com>
2023-07-20 18:23:18 -0700

    saved

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
-int main()
+int main(void)
 {
-	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19",59);
 	return (1);
 }
