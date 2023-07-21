3b9eaa0d7b145669f13dc54a65d7baee416871de
kOriTO <amr01000620@gmail.com>
2023-07-20 18:30:57 -0700

    saved

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
+/* main - Enter point
+ *
+ * Return - Always 0 (Success)
+ */
+
 int main(void)
 {
-	fwrite(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
