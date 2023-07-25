a5c33ff09d01294219142a47103c461c61a4e78e
kOriTO <amr01000620@gmail.com>
2023-07-24 19:33:15 -0700

    saved

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,6 +7,7 @@
 void print_rev(char *s)
 {
 	int len = 0, index = 0;
+
 	len = _strlen(s);
 
 	for (index = len - 1; index >= 0; index--)
@@ -23,6 +24,7 @@ void print_rev(char *s)
 int _strlen(char *s)
 {
 	int count, inc;
+
 	inc = 0;
 	for (count = 0; s[count] != '\0'; count++)
 		inc++;
